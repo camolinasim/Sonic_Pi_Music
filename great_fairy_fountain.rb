dbb76fec2791991b5d75ca8600c34ee69e10dfdf
Cristian Molina <cmutep@gmail.com>
2020-05-22 03:19:01 -0600

    Sonic Pi generated save file

---
+++ great_fairy_fountain.rb
@@ -0,0 +1,109 @@
+use_synth :piano
+use_bpm 155
+
+#first question
+1.times do
+  play :as2, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :as3, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :d4, sustain: 2, pan: -1
+  sleep 0.5
+  
+  play :a4, sustain: 2, pan: -1
+  sleep 0.5
+end
+
+#first response
+1.times do
+  sleep 2.5
+  play :d4, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :a4, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :g4, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :c5, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :a4, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :g4, sustain: 2, pan: 1
+  sleep 0.5
+  
+  play :e4, sustain: 2, pan: 1
+  sleep 0.5
+end
+
+
+#second question
+1.times do
+  
+  
+  play :a2, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :a3, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :c4, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :g4, amp: 2, pan: -1, sustain: 2
+  
+  
+end
+
+#second response
+1.times do
+  sleep 2.5
+  play :a2, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :c4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :g4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :f4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :c5, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :g4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :f4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+  
+  play :c4, amp: 2, pan: 1, sustain: 2
+  sleep 0.5
+end
+
+
+#third question
+1.times do
+  
+  play :g2, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :g3, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :as3, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+  
+  play :f4, amp: 2, pan: -1, sustain: 2
+  sleep 0.5
+end
+
+
+
