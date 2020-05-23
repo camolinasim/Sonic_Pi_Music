f6a24f75ccc96ac25b0c8854606a8ce7692e52d8
Cristian Molina <cmutep@gmail.com>
2020-05-22 18:46:49 -0600

    finished first loop. Half of the song is written.

--- great_fairy_fountain.rb
+++ great_fairy_fountain.rb
@@ -1,138 +1,182 @@
 use_synth :piano
 use_bpm 130
 
-
-#first question
-1.times do
-  play :as2, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
+loop do
+  #first question
+  1.times do
+    play :as2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :as3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :d4, sustain: 2, pan: -1
+    sleep 0.5
+    
+    play :a4, sustain: 2, pan: -1
+    sleep 0.5
+  end
   
-  play :as3, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-  
-  play :d4, sustain: 2, pan: -1
-  sleep 0.5
-  
-  play :a4, sustain: 2, pan: -1
-  sleep 0.5
-end
-
-#first response
-1.times do
-  sleep 2.5
-  play :d4, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :a4, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :g4, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :c5, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :a4, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :g4, sustain: 2, pan: 1
-  sleep 0.5
-  
-  play :e4, sustain: 2, pan: 1
-  sleep 0.5
-end
-
-
-#second question
-1.times do
+  #first response
+  1.times do
+    sleep 2.5
+    play :d4, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :a4, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :g4, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :c5, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :a4, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :g4, sustain: 2, pan: 1
+    sleep 0.5
+    
+    play :e4, sustain: 2, pan: 1
+    sleep 0.5
+  end
   
   
-  play :a2, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
+  #second question
+  1.times do
+    
+    
+    play :a2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :a3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :c4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :g4, amp: 2, pan: -1, sustain: 2
+    
+    
+  end
   
-  play :a3, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
+  #second response
+  1.times do
+    sleep 2.5
+    play :a2, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :c4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :g4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :f4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :c5, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :g4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :f4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :c4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+  end
   
-  play :c4, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
   
-  play :g4, amp: 2, pan: -1, sustain: 2
+  #third question
+  1.times do
+    
+    play :g2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :g3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :as3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :f4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+  end
   
+  #third response
+  1.times do
+    
+    sleep 2.0
+    
+    play :g2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :E4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :f4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :E4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :g4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :E4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :c4, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+    play :a3, amp: 2, pan: 1, sustain: 2
+    sleep 0.5
+    
+  end
+  #fourth question
+  1.times do
+    play :f2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :f3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :a3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :e4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+  end
   
-end
-
-#second response
-1.times do
-  sleep 2.5
-  play :a2, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :c4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :g4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :f4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :c5, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :g4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :f4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :c4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-end
-
-
-#third question
-1.times do
-  
-  play :g2, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-  
-  play :g3, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-  
-  play :as3, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-  
-  play :f4, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-end
-
-#third response
-1.times do
-  
-  sleep 2.0
-  
-  play :g2, amp: 2, pan: -1, sustain: 2
-  sleep 0.5
-  
-  play :E4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :f4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :E4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :g4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :E4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :c4, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
-  play :a3, amp: 2, pan: 1, sustain: 2
-  sleep 0.5
-  
+  #fourth response
+  1.times do
+    
+    sleep 2
+    play :f2, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :f3, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :e4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :d4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :g4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :f4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :c5, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+    
+    play :as4, amp: 2, pan: -1, sustain: 2
+    sleep 0.5
+  end
 end
